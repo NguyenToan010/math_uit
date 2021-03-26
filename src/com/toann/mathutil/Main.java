788126c1f305d4d0690d68dcc4f54783071cf978
Nguyen Toan <toannguyen00100@gmai.com>
2021-03-26 09:19:34 +0700

    Add RECURSION

--- src/com/toann/mathutil/Main.java
+++ src/com/toann/mathutil/Main.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -29,15 +29,14 @@ public class Main {
         // đệ quy 
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument ."
-                    + " n must be between 0..20");  
+                    + " n must be between 0..20");
         }
         if (n == 0 || n == 1) {
             return 1;
-        
-        } 
-        else {
-            return n * getFactorial((n - 1));
         }
+
+        return n * getFactorial((n - 1));
+
 //        long result = 1;// cố tình hen , lát tính tiếp 
 //        for (int i = 2; i < n; i++) {
 //            result *= i;
